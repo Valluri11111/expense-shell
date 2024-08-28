115927bbdd7f4473ecf785d129d31664293eef67
Valluri1111 <rajeshvalluri666@gmail.com>
2024-08-28 19:33:06 +0530

    Backend

--- backend.sh
+++ backend.sh
@@ -60,3 +60,7 @@ cd /app
 rm -rf /app/* #remove the existing code
 unzip /tmp/backend.zip &>> $LOG_FILE
 VALIDATE $? "Extracting Backend Application code"
+
+npm install &>> $LOG_FILE
+pwd
+#cp /home/ec2-user/expense-shell/backend.service/etc/systemd/system/backend.service#
